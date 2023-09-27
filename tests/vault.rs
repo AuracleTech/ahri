9d5e52da228ab8013e7f8af8aecfd7a7a9165ec2
AuracleTech <VirgilFabre@outlook.com>
2023-09-27 14:44:54 -0400

    Improve readability

--- tests/vault.rs
+++ tests/vault.rs
@@ -15,14 +15,13 @@ fn serialize_deserialize() {
     std::fs::create_dir_all(FOLDER).unwrap();
 
     let mut ahri = Vault::new();
-
     ahri.serialize(SAVE_PATH).unwrap();
     let serialized_file_size = std::fs::metadata(SAVE_PATH).unwrap().len();
 
     ahri.new_database(DATABASE_NAME).unwrap();
-
     ahri.serialize(SAVE_PATH).unwrap();
     let new_serialized_file_size = std::fs::metadata(SAVE_PATH).unwrap().len();
+
     assert_ne!(serialized_file_size, new_serialized_file_size);
 
     match Vault::deserialize(SAVE_PATH) {
